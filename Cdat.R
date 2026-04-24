4f58b391ca5b35652950d63407d97e4b8b142999
emcabana <66531410+emcabana@users.noreply.github.com>
2026-04-24 19:10:00 -0300

    Delete Cdat.R

--- Cdat.R
+++
@@ -1,6 +0,0 @@
-#' @title Precomputed C matrix
-#' @description Coefficients of the quadratic form used by TEPest
-#' @format A matrix with dimensions (l+1) x (l+1)
-#' @usage data(Cdat)
-#' @keywords internal
-"Cdat"
